60a9ef48dd4adc4e17318703a23af2631dcd5235
abdad mubaroq <abdadmubaroq@192.168.92.3>
2026-04-18 10:52:30 +0700

    migration

---
+++ database/migrations/2026_04_18_033620_create_propertis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('propertis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->constrained()->onDelete('cascade');
+            $table->string('nama');
+            $table->string('lokasi');
+            $table->text('fasilitas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('propertis');
+    }
+};

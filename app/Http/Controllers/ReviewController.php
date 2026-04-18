3c6eebeb959be4f23a4a7b33702c0a151d2b7303
abdad mubaroq <abdadmubaroq@192.168.92.3>
2026-04-18 11:09:52 +0700

    funtion login,review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
-    //
+    public function beriReview(Request $request): bool
+    {
+        Review::create([
+            'user_id' => auth()->id(),
+            'kamar_id' => $request->kamar_id,
+            'isi_review' => $request->isi_review,
+            'rating' => $request->rating
+        ]);
+
+        return true;
+    }
 }
